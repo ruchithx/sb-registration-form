9631345a73f8dfb161f8e20b814a66f360960cfc
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-01-29 12:07:59 +0530

    fix:form sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} md:h-[1170px] h-[1475px]`}>
+        {children}
+      </body>
     </html>
   );
 }
